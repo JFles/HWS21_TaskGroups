3c7f205f438585aace93c9cc0400a7cffa0ffecf
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2021-08-03 09:15:37 -0500

    Initial Commit

---
+++ Day2_Session2_TaskGroups/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day2_Session2_TaskGroups
+//
+//  Created by Jeremy Fleshman on 8/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day2_Session2_TaskGroups/Day2_Session2_TaskGroupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day2_Session2_TaskGroupsApp.swift
+//  Day2_Session2_TaskGroups
+//
+//  Created by Jeremy Fleshman on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Day2_Session2_TaskGroupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
